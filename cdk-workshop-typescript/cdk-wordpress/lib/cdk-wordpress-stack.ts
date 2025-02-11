8c7c4ad670ccb5a0e1a74c92fbea92e154853ad2
hanakonako42 <iori.42.0001@gmail.com>
2025-02-11 10:14:21 +0000

    create custom construct for web server setup

--- cdk-workshop-typescript/cdk-wordpress/lib/cdk-wordpress-stack.ts
+++ cdk-workshop-typescript/cdk-wordpress/lib/cdk-wordpress-stack.ts
@@ -1,12 +1,9 @@
 import { Stack, StackProps, aws_elasticloadbalancingv2, aws_signer } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import { readFileSync } from 'fs';
-import { CfnOutput } from 'aws-cdk-lib';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
-
 import { WebServerInstance } from './constructs/web-server-instance';
 
 export class CdkWordpressStack extends Stack {
@@ -17,22 +14,8 @@ export class CdkWordpressStack extends Stack {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     });
 
-    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
-      vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
-      machineImage: new ec2.AmazonLinuxImage( {
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
-    });
-
-    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
-    webServer1.addUserData(script);
-
-    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
-
-    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
-      value: `http://${webServer1.instancePublicIp}`,
+    const webServer1 = new WebServerInstance(this, 'WebServer1', {
+      vpc
     });
 
     const Database1 = new rds.DatabaseInstance(this, 'MysqlInstance1', {
@@ -43,7 +26,7 @@ export class CdkWordpressStack extends Stack {
       databaseName: 'wordpress'
     });
 
-    Database1.connections.allowDefaultPortFrom(Database1);
+    Database1.connections.allowDefaultPortFrom(webServer1.instance);
 
     const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
       vpc,
@@ -56,12 +39,12 @@ export class CdkWordpressStack extends Stack {
 
     listener.addTargets("ApplicationFleet", {
       port: 80,
-      targets: [new targets.InstanceTarget(webServer1, 80)],
+      targets: [new targets.InstanceTarget(webServer1.instance, 80)],
       healthCheck: {
         path: "/wp-includes/images/blank.gif",
       },
     });
 
-    webServer1.connections.allowFrom(alb, ec2.Port.tcp(80));
+    webServer1.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
   }
 }
