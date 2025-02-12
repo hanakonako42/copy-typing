2be3b95f68168230b3cae03a3ce6904254659fef
hanakonako42 <iori.42.0001@gmail.com>
2025-02-12 14:41:17 +0000

    add hello func to stack

--- cdk-immersion-day-workshop-typescript/lib/cdk-immersion-day-workshop-typescript-stack.ts
+++ cdk-immersion-day-workshop-typescript/lib/cdk-immersion-day-workshop-typescript-stack.ts
@@ -1,8 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class CdkImmersionDayWorkshopTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
   }
 }
