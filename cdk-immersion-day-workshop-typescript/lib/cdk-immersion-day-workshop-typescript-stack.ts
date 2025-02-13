dbefff448e91379efaf3e6a37952e5a614727da7
hanakonako42 <iori.42.0001@gmail.com>
2025-02-13 16:19:21 +0000

    create hit counter

--- cdk-immersion-day-workshop-typescript/lib/cdk-immersion-day-workshop-typescript-stack.ts
+++ cdk-immersion-day-workshop-typescript/lib/cdk-immersion-day-workshop-typescript-stack.ts
@@ -2,19 +2,24 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { HitCounter } from "./hitcounter";
 
 export class CdkImmersionDayWorkshopTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const hello = new Function(this, "HelloHandler", {
-      runtime: Runtime.NODEJS_18_X,
+      runtime: Runtime.NODEJS_18_X, 
       code: Code.fromAsset("lambda"),
-      handler: "hello.handler",
+      handler: "hello.handler", 
+    });
+
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
     });
 
     const gateway = new LambdaRestApi(this, "Endpoint", {
-      handler: hello,
-    })
+      handler: helloWithCounter.handler,
+    });
   }
 }
