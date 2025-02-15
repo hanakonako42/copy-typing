cb2147dbeaf4584a29dec3a2e198a7809ac74544
hanakonako42 <iori.42.0001@gmail.com>
2025-02-15 02:29:34 +0000

    create assertion test

--- cdk-immersion-day-workshop-typescript/test/hitcounter.test.ts
+++ cdk-immersion-day-workshop-typescript/test/hitcounter.test.ts
@@ -17,3 +17,51 @@ test("DynamoDB Table Created", () => {
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::DynamoDB::Table", 1);
 });
+
+test("Lambda Has Environment Variables", () => {
+  const stack = new Stack();
+
+  new HitCounter(stack, "MyTestConstruct", {
+    downstream: new Function(stack, "TestFunction", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "hello.handler",
+      code: Code.fromAsset("lambda"),
+    }),
+  });
+
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual({
+    Variables: {
+      DOWNSTREAM_FUNCTION_NAME: {
+        Ref: "TestFunction22AD90FC",
+      },
+      HITS_TABLE_NAME: {
+        Ref: "MyTestConstructHits24A357F0",
+      },
+    },
+  });
+});
+
+test("DynamoDB Table Created With Encryption", () => {
+  const stack = new Stack();
+
+  new HitCounter(stack, "MyTestConstruct", {
+    downstream: new Function(stack, "TestFunction", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "hello.handler",
+      code: Code.fromAsset("lambda"),
+    }),
+  });
+
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::DynamoDB::Table", {
+    SSESpecification: {
+      SSEEnabled: true,
+    },
+  });
+});
