5957ed6bf0d3797a02f68b6b72b8549221e53064
hanakonako42 <iori.42.0001@gmail.com>
2025-02-15 04:28:18 +0000

    configure removalpolicy to DynamoDB

--- cdk-immersion-day-workshop-typescript/lib/hitcounter.ts
+++ cdk-immersion-day-workshop-typescript/lib/hitcounter.ts
@@ -1,3 +1,4 @@
+import { RemovalPolicy } from "aws-cdk-lib";
 import { AttributeType, Table, TableEncryption } from "aws-cdk-lib/aws-dynamodb";
 import { Code, Function, IFunction, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
@@ -23,6 +24,7 @@ export class HitCounter extends Construct {
       partitionKey: { name: "path", type: AttributeType.STRING },
       encryption: TableEncryption.AWS_MANAGED,
       readCapacity: props.readCapacity ?? 5,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
     this.handler = new Function(this, "HitCounterHandler", {
