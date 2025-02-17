f278ad7e3bc89e578c2d3c81c5c3240cb58e701f
hanakonako42 <iori.42.0001@gmail.com>
2025-02-17 15:30:22 +0000

    #007 sprint1：add vpc test

---
+++ cloudtech-aws-lessons/sprint1/assignment/test/assignment.test.ts
@@ -0,0 +1,47 @@
+import * as cdk from 'aws-cdk-lib';
+import * as assertions from 'aws-cdk-lib/assertions';
+import * as Assignment from '../lib/assignment-stack';
+
+test('Vpc', () => {
+    const app = new cdk.App();
+    const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+
+    const template = assertions.Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::VPC', 1);
+    template.hasResourceProperties('AWS::EC2::VPC', {
+        CidrBlock: '10.0.0.0/21',
+        Tags: [{ 'Key': 'Name', 'Value': 'reservation-vpc'}]
+    });
+});
+
+// test('Vpcの個数が1つであること', () => {
+//     const app = new cdk.App();
+//     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+
+//     const template = assertions.Template.fromStack(stack);
+
+//     template.resourceCountIs('AWS::EC2::VPC', 1);
+// });
+
+// test('Vpcのサイダーが10.0.0.0/21であること', () => {
+//     const app = new cdk.App();
+//     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+
+//     const template = assertions.Template.fromStack(stack);
+
+//     template.hasResourceProperties('AWS::EC2::VPC', {
+//         CidrBlock: '10.0.0.0/21'
+//     });
+// });
+
+// test('VpcのNameタグがreservation-vpcであること', () => {
+//     const app = new cdk.App();
+//     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+
+//     const template = assertions.Template.fromStack(stack);
+
+//     template.hasResourceProperties('AWS::EC2::VPC', {
+//         Tags: [{ 'Key': 'Name', 'Value': 'reservation-vpc'}]
+//     });
+// });
