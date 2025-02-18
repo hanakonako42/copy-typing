48a7aa6310226a88f7e7d959ad541c240b4da5b1
hanakonako42 <iori.42.0001@gmail.com>
2025-02-18 12:45:33 +0000

    #007 sprint1：create two patterns of test files

--- cloudtech-aws-lessons/sprint1/assignment/test/assignment.test.ts
+++ cloudtech-aws-lessons/sprint1/assignment/test/assignment.test.ts
@@ -2,46 +2,33 @@ import * as cdk from 'aws-cdk-lib';
 import * as assertions from 'aws-cdk-lib/assertions';
 import * as Assignment from '../lib/assignment-stack';
 
-test('Vpc', () => {
+test('Vpcの個数が1つであること', () => {
     const app = new cdk.App();
     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
 
     const template = assertions.Template.fromStack(stack);
 
     template.resourceCountIs('AWS::EC2::VPC', 1);
+});
+
+test('Vpcのサイダーが10.0.0.0/21であること', () => {
+    const app = new cdk.App();
+    const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+
+    const template = assertions.Template.fromStack(stack);
+
     template.hasResourceProperties('AWS::EC2::VPC', {
-        CidrBlock: '10.0.0.0/21',
-        Tags: [{ 'Key': 'Name', 'Value': 'reservation-vpc'}]
+        CidrBlock: '10.0.0.0/21'
     });
 });
 
-// test('Vpcの個数が1つであること', () => {
-//     const app = new cdk.App();
-//     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+test('VpcのNameタグがreservation-vpcであること', () => {
+    const app = new cdk.App();
+    const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
 
-//     const template = assertions.Template.fromStack(stack);
+    const template = assertions.Template.fromStack(stack);
 
-//     template.resourceCountIs('AWS::EC2::VPC', 1);
-// });
-
-// test('Vpcのサイダーが10.0.0.0/21であること', () => {
-//     const app = new cdk.App();
-//     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
-
-//     const template = assertions.Template.fromStack(stack);
-
-//     template.hasResourceProperties('AWS::EC2::VPC', {
-//         CidrBlock: '10.0.0.0/21'
-//     });
-// });
-
-// test('VpcのNameタグがreservation-vpcであること', () => {
-//     const app = new cdk.App();
-//     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
-
-//     const template = assertions.Template.fromStack(stack);
-
-//     template.hasResourceProperties('AWS::EC2::VPC', {
-//         Tags: [{ 'Key': 'Name', 'Value': 'reservation-vpc'}]
-//     });
-// });
+    template.hasResourceProperties('AWS::EC2::VPC', {
+        Tags: [{ 'Key': 'Name', 'Value': 'reservation-vpc'}]
+    });
+});
