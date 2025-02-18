1c4f980434912981aa2463b281dbad593e2ecf85
hanakonako42 <iori.42.0001@gmail.com>
2025-02-18 13:40:58 +0000

    #007 sprint1：delete one test file and make two subnets

--- cloudtech-aws-lessons/sprint1/assignment/lib/assignment-stack.ts
+++ cloudtech-aws-lessons/sprint1/assignment/lib/assignment-stack.ts
@@ -6,9 +6,26 @@ export class AssignmentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new ec2.CfnVPC(this, 'reservation-vpc', {
+    const vpc = new ec2.CfnVPC(this, 'reservationVpc', {
       cidrBlock: '10.0.0.0/21',
       tags: [{ key: 'Name', value: 'reservation-vpc'}]
-    })
+    });
+
+    const webSubnet = new ec2.CfnSubnet(this, 'WebSubnet', {
+      vpcId: vpc.ref,
+      cidrBlock: '10.0.0.0/24',
+      tags: [{ key: 'Name', value: 'web-subnet-01' }]
+    });
+
+    const apiSubnet = new ec2.CfnSubnet(this, 'ApiSubnet', {
+      vpcId: vpc.ref, 
+      cidrBlock: '10.0.1.0/24',
+      tags: [ { key: 'Name', value: 'api-subnet-01' } ]
+    });
+
+    // const webRouteTable = new ec2.CfnRouteTable(this, 'WebRouteTable', {
+    //   vpcId: vpc.ref,
+
+    // })
   }
 }
--- cloudtech-aws-lessons/sprint1/assignment/test/assignment.test.ts
+++ cloudtech-aws-lessons/sprint1/assignment/test/assignment.test.ts
@@ -2,33 +2,41 @@ import * as cdk from 'aws-cdk-lib';
 import * as assertions from 'aws-cdk-lib/assertions';
 import * as Assignment from '../lib/assignment-stack';
 
-test('Vpcの個数が1つであること', () => {
+/*==================================================================
+ *vpc
+ *==================================================================*/
+test('Vpc', () => {
     const app = new cdk.App();
     const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
 
     const template = assertions.Template.fromStack(stack);
 
     template.resourceCountIs('AWS::EC2::VPC', 1);
-});
-
-test('Vpcのサイダーが10.0.0.0/21であること', () => {
-    const app = new cdk.App();
-    const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
-
-    const template = assertions.Template.fromStack(stack);
-
     template.hasResourceProperties('AWS::EC2::VPC', {
-        CidrBlock: '10.0.0.0/21'
-    });
-});
-
-test('VpcのNameタグがreservation-vpcであること', () => {
-    const app = new cdk.App();
-    const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
-
-    const template = assertions.Template.fromStack(stack);
-
-    template.hasResourceProperties('AWS::EC2::VPC', {
+        CidrBlock: '10.0.0.0/21',
         Tags: [{ 'Key': 'Name', 'Value': 'reservation-vpc'}]
     });
 });
+
+/*==================================================================
+ *subnet
+ *==================================================================*/
+test ('Subnet', () => {
+    const app = new cdk.App();
+    const stack = new Assignment.AssignmentStack(app, 'AssignmentStack');
+
+    const template = assertions.Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::Subnet', 2);
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        // CidrBlock: "10.0.0.0/24", 
+        // I'm getting an error and don't know hot to test it. I'll rewrite it once I figure it out.
+        Tags: [{ 'Key': 'Name', 'Value': 'api-subnet-01'}]
+    });
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        /* CidrBlock: '10.0.1.0/24',
+         * Tags: [{ 'key': 'Name', 'value': 'api-subnet-01'}]
+         * This test looks like cheking only first 'AWS::EC2::Subnet' type in cfn.
+         * I'm getting also an error and don't know hot to test it.*/
+    })
+})
