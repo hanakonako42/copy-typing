89c96b21e2546958c63038bf03da46b9ac18454e
hanakonako42 <iori.42.0001@gmail.com>
2025-02-22 13:08:34 +0000

    #008: #08 abstracted common functionality into abstract class

--- practical-aws-cdk/devio/lib/resource/vpc.ts
+++ practical-aws-cdk/devio/lib/resource/vpc.ts
@@ -1,18 +1,19 @@
 import { Construct } from 'constructs';
 import { CfnVPC } from 'aws-cdk-lib/aws-ec2';
+import { Resource } from '../abstract/resource';
 
-export class Vpc {
+export class Vpc extends Resource {
     public vpc: CfnVPC;
 
-    constructor() { };
+    constructor() {
+        super();
+    };
 
     public createResources(scope: Construct) {
-        const systemName = scope.node.tryGetContext('systemName');
-        const envType = scope.node.tryGetContext('envType');
 
         this.vpc = new CfnVPC(scope, 'Vpc', {
             cidrBlock: '10.0.0.0/16',
-            tags: [{ key: 'Name', value: `${systemName}-${envType}-vpc`}]
+            tags: [{ key: 'Name', value: this.createResourceName(scope, 'vpc') }]
         });
     }   
 }
